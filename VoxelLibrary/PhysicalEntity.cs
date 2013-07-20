3e0475da33dd18338df1630c649bc5ee1027d72e
Justin Dubs <jtdubs@gmail.com>
2013-07-19 23:26:08 -0400

    missed PE and Scene classes

---
+++ VoxelLibrary/PhysicalEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VoxelLand
+{
+    public class PhysicalEntity : Entity
+    {
+        public PhysicalEntity(Mesh mesh, Material material, CoordinateSystem coordinateSystem=null, string name="")
+            : base(coordinateSystem, name)
+        {
+            Mesh = mesh;
+            Material = material;
+        }
+
+        public Mesh Mesh { get; private set; }
+        public Material Material { get; private set; }
+    }
+}
---
+++ VoxelLibrary/Scene.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VoxelLand
+{
+    public class Scene : IEnumerable<Entity>
+    {
+        public Scene()
+        {
+            entities = new List<Entity>();
+        }
+
+        public void Add(Entity e)
+        {
+            entities.Add(e);
+        }
+
+        public IEnumerator<Entity> GetEnumerator()
+        {
+            return entities.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private List<Entity> entities;
+    }
+}
